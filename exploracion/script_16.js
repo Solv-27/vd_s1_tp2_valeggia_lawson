197d101cc584a1686e54ba2a407a12cf7b32c97b
luisalawson <79951357+luisalawson@users.noreply.github.com>
2023-04-05 01:47:18 -0400

    Create script_16.js

---
+++ exploracion/script_16.js
@@ -0,0 +1,30 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+    let chart_16 = Plot.plot({
+      marks:[
+      Plot.barY(
+        data, 
+        Plot.groupX(
+        {y:"mean"},
+        {y:'mision_hs',
+        x:'ocupacion', fill:'genero'
+      }
+        )),
+      ],
+      grid: true,
+      color:{
+        legend: true,
+        scheme: 'set2',
+  
+      },
+      y:{
+        label: "Horas por Misión"
+      },
+      x:{
+        tickRotate:-15,
+        label: "Ocupación"
+      },
+      marginBottom: 80
+    }
+    )
+    d3.select('#chart_16').append(() => chart_16)
+  })
